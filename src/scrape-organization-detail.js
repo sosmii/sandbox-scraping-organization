040fbef76b4805779b370581a0c407d3cde09372
sosmii <setsuya.osumi.private@gmail.com>
2018-11-16 11:38:52 +0900

    fdsg

--- src/scrape-organization-detail.js
+++ src/scrape-organization-detail.js
@@ -2,7 +2,6 @@
 
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-// const readline = require('readline');
 const LineByLineReader = require('line-by-line');
 
 const consts = require('./lib/consts');
